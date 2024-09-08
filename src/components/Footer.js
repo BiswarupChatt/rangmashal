87ad0d2c2e7a234fad9e506e1470bcaaacc8a470
Biswarup Chatterjee <chatterjeebiswarup61@gmail.com>
2024-09-08 23:35:31 +0530

    footer edit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,8 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
+import { LineWeight } from '@mui/icons-material';
 
 export default function Footer() {
 
@@ -40,7 +41,7 @@ export default function Footer() {
                     />
                 </Grid>
 
-                <Grid item xs={12} md={3} textAlign="left">
+                <Grid item xs={12} md={3} sx={{ textAlign: { xs: 'center', md: 'left' } }}>
                     <Typography variant="h6" gutterBottom>
                         Quick Links
                     </Typography>
@@ -60,39 +61,45 @@ export default function Footer() {
                     ))}
                 </Grid>
 
-                <Grid item xs={12} md={3} textAlign="left">
+                <Grid item xs={12} md={3} sx={{ textAlign: { xs: 'center', md: 'left' } }}>
                     <Typography variant="h6" gutterBottom>
                         Contact
                     </Typography>
-                    <Typography variant="body2" color="textSecondary" gutterBottom sx={{ lineHeight: 1.6 }}>
-                        Address: <br />
-                        Rangmashal Foundation Office, <br />
-                        Kalitala Kayalpara Panchasayar, <br />
-                        Garia, West Bengal, India <br />
-                        Pin Code: 700-084
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" gutterBottom sx={{ mt: 2 }}>
-                        Email: <br />
+                    <Typography variant="body2" color="textSecondary" gutterBottom>
+                        <span style={{ fontWeight: 'bold' }}>
+                            Email: <br />
+                        </span>
                         <a href="mailto:connecttorangmashal@gmail.com" style={{ color: 'inherit', textDecoration: 'none' }}>
                             connecttorangmashal@gmail.com
                         </a>
                     </Typography>
                     <Typography variant="body2" color="textSecondary" sx={{ mt: 2 }}>
-                        Phone: <br />
+                        <span style={{ fontWeight: 'bold' }}>
+                            Phone: <br />
+                        </span>
                         <a href="tel:+916290442860" style={{ color: 'inherit', textDecoration: 'none' }}>
                             +91 62904 42860
                         </a>
                     </Typography>
+                    <Typography variant="body2" color="textSecondary" gutterBottom sx={{ mt: 2 }}>
+                        <span style={{ fontWeight: 'bold' }}>
+                            Address: <br />
+                        </span>
+                        Rangmashal Foundation Office, <br />
+                        Kalitala Kayalpara Panchasayar, <br />
+                        Garia, West Bengal, India <br />
+                        Pin Code: 700-084
+                    </Typography>
                 </Grid>
 
 
-                <Grid item xs={12} md={3} textAlign="left">
+                <Grid item xs={12} md={3} sx={{ textAlign: { xs: 'center', md: 'left' } }}>
                     <Typography variant="h6" gutterBottom>
                         Follow Us
                     </Typography>
                     <Box>
                         {socialLinks.map((social, index) => (
-                            <IconButton key={index} component={Link} to={social.path} sx={{ color: 'primary.main', padding:'0px 15px 0px 0px' }}>
+                            <IconButton key={index} component={Link} to={social.path} sx={{ color: 'primary.main', padding: '0px 15px 0px 0px' }}>
                                 {social.icon}
                             </IconButton>
                         ))}

79079a144c828a40c0b34749cb87601304480feb
Biswarup Chatterjee <chatterjeebiswarup61@gmail.com>
2024-09-14 00:05:59 +0530

    project update

--- src/components/projects/AllProjects.js
+++ src/components/projects/AllProjects.js
@@ -1,58 +1,9 @@
-// import BloodDonationCamp from "./projectDetails/BloodDonationCamp"
-// import AmphanReliefWork from "./projectDetails/AmphanReliefWork"
 
 import ProjectDetails from "./ProjectDetails"
+import eventsData from "./eventsData";
 
-const eventsData = [
-    {
-        title: 'Blood Donation Camp',
-        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae fugiat aliquid itaque aspernatur rem commodi non, modi consequuntur quia natus quis repellendus distinctio, reiciendis dolore obcaecati neque culpa atque. Magnam quibusdam eum dicta, voluptas neque perferendis? Quidem sint debitis, sunt omnis hic sequi provident atque officiis distinctio at quod rem.',
-        subEvents: [
-            {
-                subTitle: 'Blood Donation Camp: Part 1',
-                subDescription: 'On the occasion of International Mother Language Day to pay homage to our brave martyrs, we organized a blood donation camp in association with Ashok Laboratory at Subodhpark Saptarshi Friend’s Association club. This was our very first project after Government registration as Rangmashal Foundation.',
-                date: '21st February, 2021',
-                images: [
-                    'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899302/Website/nt81qnkkkvjcch48v53k.jpg',
-                    'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899112/Website/lwijqhxavefwc1ftue0e.jpg',
-                ],
-            },
-            {
-                subTitle: 'Blood Donation Camp: Part 2',
-                subDescription: 'Rangmashal Foundation organized a blood donation camp in association with Baruipur Sub-divisional Hospital at Garfa North Star Association. It was our second Blood Donation Camp after the first on 21st February 2021.',
-                date: '16th October',
-                images: [
-                    'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899302/Website/nt81qnkkkvjcch48v53k.jpg',
-                    'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899112/Website/lwijqhxavefwc1ftue0e.jpg',
-                ],
-            },
-        ],
-    },
-    {
-        title: 'Amphan Relief Work',
-        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae fugiat aliquid itaque aspernatur rem commodi non, modi consequuntur quia natus quis repellendus distinctio, reiciendis dolore obcaecati neque culpa atque. Magnam quibusdam eum dicta, voluptas neque perferendis? Quidem sint debitis, sunt omnis hic sequi provident atque officiis distinctio at quod rem.',
-        subEvents: [
-            {
-                subTitle: 'Kumartuli',
-                subDescription: 'On 2nd June, 2020 we extended our hands to Kumartuli, which was devastated by the cyclone “Amphan”. There we distributed near about 50 packets of ration. In this project we were assisted by Mr. Apurba Pal, one of the sculptors of the Kumartuli. He was a great help to us for that project.',
-                date: '2nd June, 2020',
-                images: [
-                    'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899302/Website/nt81qnkkkvjcch48v53k.jpg',
-                    'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899112/Website/lwijqhxavefwc1ftue0e.jpg',
-                ],
-            },
-            {
-                subTitle: 'Sundarban',
-                subDescription: 'We also extended our hands towards the Sundarban, which was devastated by the cyclone Amphan. Our members went there and gave away day to day necessary items like medicine, sanitary napkins and other items. We also gave away new clothes and a packet of ration to the people there.',
-                date: '14th June, 2020',
-                images: [
-                    'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899302/Website/nt81qnkkkvjcch48v53k.jpg',
-                    'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899112/Website/lwijqhxavefwc1ftue0e.jpg',
-                ],
-            },
-        ],
-    },
-];
+
+
 
 
 export default function AllProjects() {
---
+++ src/components/projects/eventsData.js
@@ -0,0 +1,76 @@
+const eventsData = [
+    {
+        title: 'Blood Donation Camp',
+        description: 'Our blood donation initiatives include two impactful camps aimed at contributing to the community. The first was organized in February 2021, on International Mother Language Day, in collaboration with Ashok Laboratory, and the second in October 2021 with Baruipur Sub-divisional Hospital. Both camps had remarkable success in saving lives and promoting the importance of blood donations.',
+        subEvents: [
+            {
+                subTitle: 'Blood Donation Camp: Part 1',
+                subDescription: 'On the occasion of International Mother Language Day to pay homage to our brave martyrs, we organized a blood donation camp in association with Ashok Laboratory at Subodhpark Saptarshi Friend’s Association club. This was our very first project after Government registration as Rangmashal Foundation.',
+                date: '21st February, 2021',
+                images: [
+                    'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899302/Website/nt81qnkkkvjcch48v53k.jpg',
+                    'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899112/Website/lwijqhxavefwc1ftue0e.jpg',
+                ],
+            },
+            {
+                subTitle: 'Blood Donation Camp: Part 2',
+                subDescription: 'Rangmashal Foundation organized a blood donation camp in association with Baruipur Sub-divisional Hospital at Garfa North Star Association. It was our second Blood Donation Camp after the first on 21st February 2021.',
+                date: '16th October',
+                images: [
+                    'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899302/Website/nt81qnkkkvjcch48v53k.jpg',
+                    'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899112/Website/lwijqhxavefwc1ftue0e.jpg',
+                ],
+            },
+        ],
+    },
+    {
+        title: 'Amphan Relief Work',
+        description: 'Our efforts during the Amphan cyclone relief work focused on providing critical support to two key areas: Kumartuli and Sundarban. In June 2020, we distributed ration and essential items to sculptors in Kumartuli and the residents of Sundarban who were severely affected by the cyclone.',
+        subEvents: [
+            {
+                subTitle: 'Kumartuli',
+                subDescription: 'On 2nd June, 2020 we extended our hands to Kumartuli, which was devastated by the cyclone “Amphan”. There we distributed near about 50 packets of ration. In this project we were assisted by Mr. Apurba Pal, one of the sculptors of the Kumartuli. He was a great help to us for that project.',
+                date: '2nd June, 2020',
+                images: [
+                    'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899302/Website/nt81qnkkkvjcch48v53k.jpg',
+                    'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899112/Website/lwijqhxavefwc1ftue0e.jpg',
+                ],
+            },
+            {
+                subTitle: 'Sundarban',
+                subDescription: 'We also extended our hands towards the Sundarban, which was devastated by the cyclone Amphan. Our members went there and gave away day to day necessary items like medicine, sanitary napkins and other items. We also gave away new clothes and a packet of ration to the people there.',
+                date: '14th June, 2020',
+                images: [
+                    'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899302/Website/nt81qnkkkvjcch48v53k.jpg',
+                    'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899112/Website/lwijqhxavefwc1ftue0e.jpg',
+                ],
+            },
+        ],
+    },
+    {
+        title: 'Yaas Relief Work',
+        description: 'Our relief efforts during the aftermath of Cyclone Yaas focused on supporting the communities in Sundarban and Mandarmani. Through compassion and care, we helped over 750 individuals by providing food, clothing, and essential assistance in these affected regions.',
+        subEvents: [
+            {
+                subTitle: 'Sundarban-e Rangmashal',
+                subDescription: 'In Sundarbans, we extended warmth and compassion by distributing food and clothes to 500 people. This simple act aimed to bring relief to those in need, fostering a sense of community and solidarity in the heart of the picturesque Sundarbans region.',
+                date: 'Sundarbans, 2021',
+                images: [
+                    'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899302/Website/nt81qnkkkvjcch48v53k.jpg',
+                    'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899112/Website/lwijqhxavefwc1ftue0e.jpg',
+                ],
+            },
+            {
+                subTitle: 'Untouched Mandarmani',
+                subDescription: 'Our visit to Mandarmani was marked by a heartfelt mission to aid 250 individuals. With a spirit of compassion, we provided essential assistance, ensuring that those in need felt supported. Whether through food, clothing, or other forms of assistance, our goal was to make a positive impact in the lives of the people of Mandarmani, fostering a sense of community and solidarity in this coastal haven.',
+                date: 'Mandarmani, 2021',
+                images: [
+                    'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899302/Website/nt81qnkkkvjcch48v53k.jpg',
+                    'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899112/Website/lwijqhxavefwc1ftue0e.jpg',
+                ],
+            },
+        ],
+    }
+];
+
+export default eventsData

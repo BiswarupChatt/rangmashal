11411d032af778bec4c89daddbe084847332c55e
Biswarup Chatterjee <chatterjeebiswarup61@gmail.com>
2024-09-20 23:23:08 +0530

    changes v2

--- src/components/createMemories/Index.js
+++ src/components/createMemories/Index.js
@@ -1,12 +1,14 @@
 // import ComingSoon from "../ComingSoon";
 import Hero from "./Hero";
 import TabSection from "./TabSection";
+import Contact from "./Contact";
 
 export default function Index() {
     return (
         <>
             <Hero />
             <TabSection />
+            <Contact />
         </>
     )
 }

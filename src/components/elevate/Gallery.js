01012985ff2e1392ef79959e2479c4b579ffb85c
Biswarup Chatterjee <chatterjeebiswarup61@gmail.com>
2024-09-24 23:56:50 +0530

    elevate update

--- src/components/elevate/Gallery.js
+++ src/components/elevate/Gallery.js
@@ -7,32 +7,17 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Navigation, Pagination, Autoplay, EffectCoverflow } from 'swiper/modules';
 import CloseIcon from '@mui/icons-material/Close';
+import { Slide, Fade } from 'react-awesome-reveal';
 
 const galleryImages = [
-    {
-        title: 'Image 1',
-        description: 'This is the description for image 1',
-        image: 'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899112/Website/lwijqhxavefwc1ftue0e.jpg',
-        date: '01/01/2024',
-    },
-    {
-        title: 'Image 2',
-        description: 'This is the description for image 2',
-        image: 'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899302/Website/nt81qnkkkvjcch48v53k.jpg',
-        date: '02/01/2024',
-    },
-    {
-        title: 'Image 3',
-        description: 'This is the description for image 3',
-        image: 'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899112/Website/lwijqhxavefwc1ftue0e.jpg',
-        date: '03/01/2024',
-    },
-    {
-        title: 'Image 2',
-        description: 'This is the description for image 2',
-        image: 'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899302/Website/nt81qnkkkvjcch48v53k.jpg',
-        date: '02/01/2024',
-    },
+    { image: 'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899302/Website/nt81qnkkkvjcch48v53k.jpg', },
+    { image: 'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899112/Website/lwijqhxavefwc1ftue0e.jpg', },
+    { image: 'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899302/Website/nt81qnkkkvjcch48v53k.jpg', },
+    { image: 'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899112/Website/lwijqhxavefwc1ftue0e.jpg', },
+    { image: 'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899302/Website/nt81qnkkkvjcch48v53k.jpg', },
+    { image: 'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899112/Website/lwijqhxavefwc1ftue0e.jpg', },
+    { image: 'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899302/Website/nt81qnkkkvjcch48v53k.jpg', },
+    { image: 'https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899112/Website/lwijqhxavefwc1ftue0e.jpg', },
 ];
 
 export default function Gallery() {
@@ -51,63 +36,70 @@ export default function Gallery() {
     return (
         <Box sx={{ width: '100%', backgroundColor: 'rgba(255, 208, 65, 0.2)' }}>
             <Container sx={{ padding: { xs: '20px', md: '50px' } }}>
+                <Slide direction="up" cascade damping={1} triggerOnce>
+                    <Fade cascade triggerOnce>
+                        <Box>
+                            <Typography variant="h4" sx={{ fontWeight: 'bold', textAlign: 'center', mb: 4 }}>
+                                Gallery
+                            </Typography>
 
-                <Typography variant="h4" sx={{ fontWeight: 'bold', textAlign: 'center', mb: 4 }}>
-                    Gallery
-                </Typography>
-
-                <Swiper
-                    modules={[Navigation, Pagination, Autoplay]}
-                    spaceBetween={15}
-                    navigation
-                    pagination={{ clickable: true }}
-                    autoplay={{ delay: 6000, disableOnInteraction: false }}
-                    effect={'coverflow'}
-                    grabCursor={true}
-                    loop={true}
-                    speed={1500}
-                    breakpoints={{
-                        640: { slidesPerView: 1 },
-                        768: { slidesPerView: 2 },
-                        1024: { slidesPerView: 3 },
-                    }}
-                >
-                    {galleryImages.map((ele, index) => (
-                        <SwiperSlide key={index}>
-                            <Box
-                                onClick={() => handleClickOpen(index)}
-                                sx={{
-                                    width: '100%',
-                                    height: '200px',
-                                    borderRadius: '20px',
-                                    overflow: 'hidden',
-                                    cursor: 'pointer',
-                                    mb: 5
+                            <Swiper
+                                modules={[Navigation, Pagination, Autoplay]}
+                                spaceBetween={15}
+                                navigation
+                                pagination={{ clickable: true }}
+                                autoplay={{ delay: 6000, disableOnInteraction: false }}
+                                effect={'coverflow'}
+                                grabCursor={true}
+                                loop={true}
+                                speed={1500}
+                                breakpoints={{
+                                    640: { slidesPerView: 1 },
+                                    768: { slidesPerView: 2 },
+                                    1024: { slidesPerView: 3 },
                                 }}
                             >
-                                <img
-                                    src={ele.image}
-                                    alt={ele.title}
-                                    style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-                                />
-                                <Box
-                                    sx={{
-                                        position: 'absolute',
-                                        top: 0,
-                                        left: 0,
-                                        width: '100%',
-                                        height: '100%',
-                                        color: 'white',
-                                        zIndex: 2,
-                                        display: 'flex',
-                                        flexDirection: 'column',
-                                    }}
-                                >
-                                </Box>
-                            </Box>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
+                                {galleryImages.map((ele, index) => (
+                                    <SwiperSlide key={index}>
+                                        <Box
+                                            onClick={() => handleClickOpen(index)}
+                                            sx={{
+                                                width: '100%',
+                                                height: '200px',
+                                                borderRadius: '20px',
+                                                overflow: 'hidden',
+                                                cursor: 'pointer',
+                                                mb: 5
+                                            }}
+                                        >
+                                            <img
+                                                src={ele.image}
+                                                alt={ele.title}
+                                                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                                            />
+                                            <Box
+                                                sx={{
+                                                    position: 'absolute',
+                                                    top: 0,
+                                                    left: 0,
+                                                    width: '100%',
+                                                    height: '100%',
+                                                    color: 'white',
+                                                    zIndex: 2,
+                                                    display: 'flex',
+                                                    flexDirection: 'column',
+                                                }}
+                                            >
+                                            </Box>
+                                        </Box>
+                                    </SwiperSlide>
+                                ))}
+                            </Swiper>
+                        </Box>
+                    </Fade>
+                </Slide >
+
+
 
 
                 <Dialog open={open} onClose={handleClose} maxWidth="lg" fullWidth>
--- src/components/elevate/WantToHelp.js
+++ src/components/elevate/WantToHelp.js
@@ -1,35 +1,39 @@
 import React from 'react';
 import { Box, Typography, Container } from '@mui/material';
+import { Slide, Fade } from 'react-awesome-reveal';
 
 export default function WantToHelp() {
   return (
     <Box sx={{ py: 8, backgroundColor: 'rgba(50, 191, 194, 0.1)' }}>
       <Container>
-        <Typography variant="h4" align="center" sx={{ fontWeight: 'bold', mb: 2, }}>
-          Want to Help <Box component="span" sx={{ color: 'primary.main' }}>Children?</Box>
-        </Typography>
-        
-        <Typography variant="body1" align="center" sx={{ mb: 3, color: 'text.secondary', textAlign: 'justify', textAlignLast: 'left' }}>
-          Are you trying to find an NGO in India that helps underprivileged children through education? Rangmashal is here to make a real difference. We are committed to giving children from disadvantaged backgrounds the education they deserve, with programs that create lasting change and open doors to a brighter future.
-          <br /><br />
-          Rangmashal is not just transforming lives—it’s shaping a generation of empowered, educated, and capable young individuals ready to lead the future. Join us in making this vision a reality!
-        </Typography>
-        <Box
-          component="img"
-          sx={{
-            width: '100%',
-            height: 'auto',
-            borderRadius: '8px',
-            boxShadow: '8px 8px 15px rgba(0, 0, 0, 0.4)',
-            marginBottom: '20px'
-          }}
-          src="https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899302/Website/nt81qnkkkvjcch48v53k.jpg"
-          alt="Genesis of Rangmashal"
-        />
-        <Typography variant="body1" align="center" sx={{ mt: 3, color: 'text.secondary' }}>
+        <Slide direction="up" cascade damping={1} triggerOnce>
+          <Fade cascade triggerOnce>
+            <Box>
+              <Typography variant="h4" align="center" sx={{ fontWeight: 'bold', mb: 2, }}>
+                Want to Help <Box component="span" sx={{ color: 'primary.main' }}>Children?</Box>
+              </Typography>
 
-        </Typography>
+              <Typography variant="body1" align="center" sx={{ mb: 3, color: 'text.secondary', textAlign: 'justify', textAlignLast: 'left' }}>
+                Are you trying to find an NGO in India that helps underprivileged children through education? Rangmashal is here to make a real difference. We are committed to giving children from disadvantaged backgrounds the education they deserve, with programs that create lasting change and open doors to a brighter future.
+                <br /><br />
+                Rangmashal is not just transforming lives—it’s shaping a generation of empowered, educated, and capable young individuals ready to lead the future. Join us in making this vision a reality!
+              </Typography>
+              <Box
+                component="img"
+                sx={{
+                  width: '100%',
+                  height: 'auto',
+                  borderRadius: '8px',
+                  boxShadow: '8px 8px 15px rgba(0, 0, 0, 0.4)',
+                  marginBottom: '20px'
+                }}
+                src="https://res.cloudinary.com/dgwgnfulm/image/upload/v1725899302/Website/nt81qnkkkvjcch48v53k.jpg"
+                alt="Genesis of Rangmashal"
+              />
+            </Box>
+          </Fade>
+        </Slide>
       </Container>
-    </Box>
+    </Box >
   );
 }

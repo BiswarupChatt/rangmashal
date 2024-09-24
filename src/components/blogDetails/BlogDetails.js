d7d5521152f5e4d202d58030fd3da08e76a65b11
Biswarup Chatterjee <chatterjeebiswarup61@gmail.com>
2024-09-24 17:48:08 +0530

    blog details update

--- src/components/blogDetails/BlogDetails.js
+++ src/components/blogDetails/BlogDetails.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { Box, Typography, Container, Paper } from '@mui/material';
+import { Box, Typography, Container, Divider } from '@mui/material';
 import { blogPosts } from '../blog/blogData';
 import PageNotFound from '../../pages/PageNotFound';
+import parse from 'html-react-parser'
 
 export default function BlogDetails() {
     const { id } = useParams();
@@ -12,59 +13,63 @@ export default function BlogDetails() {
         <Container sx={{ py: 4 }}>
 
 
-                {post ? (
-                    <>
-                        <Box sx={{ position: 'relative', mb: 4 }}>
-                            <img
-                                src={post.image}
-                                alt={post.title}
-                                style={{ width: '100%', height: 'auto', borderRadius: '12px' }}
-                            />
-                        </Box>
-
-                        <Typography
-                            variant="h3"
-                            sx={{
-                                fontWeight: 'bold',
-                                mb: 2,
-                                textAlign: 'center',
-                                fontSize: { xs: '1.75rem', sm: '2.5rem', md: '3rem' },
-                            }}
-                        >
-                            {post.title}
-                        </Typography>
-
-                        <Typography
-                            variant="subtitle2"
-                            sx={{
-                                mb: 4,
-                                textAlign: 'center',
-                                color: 'text.secondary',
-                                fontSize: { xs: '0.875rem', sm: '1rem', md: '1.125rem' },
-                            }}
-                        >
-                            {post.date}
-                        </Typography>
-
-                        <Box
-                            dangerouslySetInnerHTML={{ __html: post.content }}
-                            sx={{
-                                fontSize: { xs: '1rem', sm: '1.125rem', md: '1.25rem' },
-                                lineHeight: 1.7,
-                                color: 'text.primary',
-                                maxWidth: '800px',
-                                margin: '0 auto',
-                            }}
+            {post ? (
+                <>
+                    <Box sx={{ position: 'relative', mb: 4 }}>
+                        <img
+                            src={post.image}
+                            alt={post.title}
+                            style={{ width: '100%', height: 'auto', borderRadius: '12px' }}
                         />
-                    </>
-                ) : (
-                    <Box>
-                        <Typography variant="h4" align="center" fontWeight='bold' color='textSecondary'>
-                            Blog Post Not Found
-                        </Typography>
-                        <PageNotFound />
                     </Box>
-                )}
+
+                    <Typography
+                        variant="h3"
+                        sx={{
+                            fontWeight: 'bold',
+                            mb: 2,
+                            textAlign: 'center',
+                            fontSize: { xs: '1.75rem', sm: '2.5rem', md: '3rem' },
+                        }}
+                    >
+                        {post.title}
+                    </Typography>
+
+                    <Typography
+                        variant="subtitle2"
+                        sx={{
+                            mb: 4,
+                            textAlign: 'center',
+                            color: 'text.secondary',
+                            fontSize: { xs: '0.875rem', sm: '1rem', md: '1.125rem' },
+                        }}
+                    >
+                        {post.date}
+                    </Typography>
+
+                    <Divider />
+
+                    <Box
+                        sx={{
+                            fontSize: { xs: '1rem', sm: '1.125rem', md: '1.25rem' },
+                            textAlign: 'justify',
+                            lineHeight: 1.7,
+                            color: 'text.primary',
+                            maxWidth: '800px',
+                            margin: '0 auto',
+                        }}
+                    >
+                        {parse(post.content)}
+                    </Box>
+                </>
+            ) : (
+                <Box>
+                    <Typography variant="h4" align="center" fontWeight='bold' color='textSecondary'>
+                        Blog Post Not Found
+                    </Typography>
+                    <PageNotFound />
+                </Box>
+            )}
         </Container>
     );
 }

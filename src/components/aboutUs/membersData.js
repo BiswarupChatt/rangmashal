3b2bab7dee99e35b621a8af23898a89d2cf744e6
Biswarup Chatterjee <chatterjeebiswarup61@gmail.com>
2024-09-28 20:48:07 +0530

    membersData update

--- src/components/aboutUs/membersData.js
+++ src/components/aboutUs/membersData.js
@@ -1,83 +1,92 @@
 export const membersData = [
-    {
-        categoryName: "Volunteer",
-        members: [
-            {
-                name: 'Tirthankar Sarkar',
-                position: 'President',
-                imageUrl: 'https://media.licdn.com/dms/image/v2/D5603AQFXL05uliNGdA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1718241558096?e=2147483647&v=beta&t=NJg23Vc5H28LmkrEdxFakeBwq2tbDkipWMrjq_F5TpE',
-                description: `Tirthankar Sarkar, a distinguished professional holding a master's degree in economics from the University of Calcutta, presently serves as an Analyst at Ipsos MMA. Renowned for his proactive engagement in social issues, Tirthankar has actively participated in various conferences since his school days, showcasing a profound interest in societal development. With a robust academic background and a steadfast commitment to social causes, he contributes invaluable resources to the Rangmashal Foundation.`,
-                linkedIn: 'https://www.linkedin.com/in/tirthankar-sarkar21/',
-                facebook: 'https://www.facebook.com/tirthankarsarkar21',
-                instagram: 'https://www.instagram.com/itsyourtirtha/?hl=en'
-            },
-            {
-                name: 'Turja Chowdhury',
-                position: 'Vice President',
-                imageUrl: '',
-                description: `Turja Chowdhury, a Jadavpur University Philosophy Postgraduate, ardently supports the Rangmashal Foundation's grassroots initiatives. Currently pursuing a PG in Journalism, his adept understanding of societal intricacies fuels his proactive engagement. With a passion for social change, Turja seamlessly integrates academic insights with practical endeavors for a better tomorrow.`,
-                linkedIn: '',
-                facebook: 'https://www.facebook.com/turja.chowdhury.3',
-                instagram: 'https://www.instagram.com/turja______/?hl=en'
-            },
-            {
-                name: 'Moutushi Mitra',
-                position: 'Resource Allocation',
-                imageUrl: '',
-                description: `I am a sincere, honest, and hardworking individual now engaged with PNB Metlife India as an Insurance Distribution Manager. I lead a team of insurance advisors and am responsible for driving agency channel sales through my team. As director of Resource Mobilization at Rangmashal, I am responsible for generating funds, finding and mobilizing skilled personnel for social projects, and networking with other organizations that are willing to support our cause.`,
-                linkedIn: 'https://www.linkedin.com/in/moutushi-mitra-57093b52/',
-                facebook: 'https://www.facebook.com/moutushi.mitra.52',
-                instagram: 'https://www.instagram.com/moutushi.1703/'
-            },
-            {
-                name: 'Moutushi Mitra',
-                position: 'Resource Allocation',
-                imageUrl: '',
-                description: `I am a sincere, honest, and hardworking individual now engaged with PNB Metlife India as an Insurance Distribution Manager. I lead a team of insurance advisors and am responsible for driving agency channel sales through my team. As director of Resource Mobilization at Rangmashal, I am responsible for generating funds, finding and mobilizing skilled personnel for social projects, and networking with other organizations that are willing to support our cause.`,
-                linkedIn: 'https://www.linkedin.com/in/moutushi-mitra-57093b52/',
-                facebook: 'https://www.facebook.com/moutushi.mitra.52',
-                instagram: 'https://www.instagram.com/moutushi.1703/'
-            },
-        ]
-    },
+    // {
+    //     categoryName: "Volunteer",
+    //     members: [
+    //         {
+    //             name: 'Tirthankar Sarkar',
+    //             position: 'President',
+    //             imageUrl: 'https://media.licdn.com/dms/image/v2/D5603AQFXL05uliNGdA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1718241558096?e=2147483647&v=beta&t=NJg23Vc5H28LmkrEdxFakeBwq2tbDkipWMrjq_F5TpE',
+    //             description: `Tirthankar Sarkar, a distinguished professional holding a master's degree in economics from the University of Calcutta, presently serves as an Analyst at Ipsos MMA. Renowned for his proactive engagement in social issues, Tirthankar has actively participated in various conferences since his school days, showcasing a profound interest in societal development. With a robust academic background and a steadfast commitment to social causes, he contributes invaluable resources to the Rangmashal Foundation.`,
+    //             linkedIn: 'https://www.linkedin.com/in/tirthankar-sarkar21/',
+    //             facebook: 'https://www.facebook.com/tirthankarsarkar21',
+    //             instagram: 'https://www.instagram.com/itsyourtirtha/?hl=en'
+    //         },
+    //         {
+    //             name: 'Turja Chowdhury',
+    //             position: 'Vice President',
+    //             imageUrl: '',
+    //             description: `Turja Chowdhury, a Jadavpur University Philosophy Postgraduate, ardently supports the Rangmashal Foundation's grassroots initiatives. Currently pursuing a PG in Journalism, his adept understanding of societal intricacies fuels his proactive engagement. With a passion for social change, Turja seamlessly integrates academic insights with practical endeavors for a better tomorrow.`,
+    //             linkedIn: '',
+    //             facebook: 'https://www.facebook.com/turja.chowdhury.3',
+    //             instagram: 'https://www.instagram.com/turja______/?hl=en'
+    //         },
+    //         {
+    //             name: 'Moutushi Mitra',
+    //             position: 'Resource Allocation',
+    //             imageUrl: '',
+    //             description: `I am a sincere, honest, and hardworking individual now engaged with PNB Metlife India as an Insurance Distribution Manager. I lead a team of insurance advisors and am responsible for driving agency channel sales through my team. As director of Resource Mobilization at Rangmashal, I am responsible for generating funds, finding and mobilizing skilled personnel for social projects, and networking with other organizations that are willing to support our cause.`,
+    //             linkedIn: 'https://www.linkedin.com/in/moutushi-mitra-57093b52/',
+    //             facebook: 'https://www.facebook.com/moutushi.mitra.52',
+    //             instagram: 'https://www.instagram.com/moutushi.1703/'
+    //         },
+    //         {
+    //             name: 'Moutushi Mitra',
+    //             position: 'Resource Allocation',
+    //             imageUrl: '',
+    //             description: `I am a sincere, honest, and hardworking individual now engaged with PNB Metlife India as an Insurance Distribution Manager. I lead a team of insurance advisors and am responsible for driving agency channel sales through my team. As director of Resource Mobilization at Rangmashal, I am responsible for generating funds, finding and mobilizing skilled personnel for social projects, and networking with other organizations that are willing to support our cause.`,
+    //             linkedIn: 'https://www.linkedin.com/in/moutushi-mitra-57093b52/',
+    //             facebook: 'https://www.facebook.com/moutushi.mitra.52',
+    //             instagram: 'https://www.instagram.com/moutushi.1703/'
+    //         },
+    //     ]
+    // },
     {
         categoryName: "Board of Members",
         members: [
             {
-                name: 'Sukanya Guha',
-                position: 'Secretary',
-                imageUrl: 'https://media.licdn.com/dms/image/v2/C5603AQEneYLNyRS06A/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1640772448605?e=2147483647&v=beta&t=-B3bT2VxPiXX1o7FWiboYKXZjIc7suhQ9JTQiwROqbU',
-                description: `I am Sukanya Guha, an Analyst by profession, currently working in a private company located in Bangalore. I hold a master's degree in Economics. I have been a part of the Rangmashal Foundation since its inception and also one of the founding members of this organization. Being the secretary of this organization, I have been handling all the internal matters of it since the very beginning. My motive is to make this world a better place to live in for all and through the Rangmashal Foundation I am being able to do so.`,
-                linkedIn: 'https://www.linkedin.com/in/sukanya-guha-671951218/',
-                facebook: 'https://www.facebook.com/sukanya.guha.79',
-                instagram: 'https://www.instagram.com/sukanya_guha_/?hl=en'
+                name: 'Ayush Mondal',
+                position: 'Co-Founder and Secretary',
+                imageUrl: 'https://res.cloudinary.com/dgwgnfulm/image/upload/v1727534000/Website/partners-logo/members-image/ayush-mondal.png',
+                description: `Ayush holds a Master's degree in Economics from Visva-Bharati, Santiniketan, with a specialization in Development Economics. He believes that education is a fundamental means of expanding individual freedoms, enhancing capabilities, and enabling people to overcome socio-economic barriers to lead lives they value. Currently based in Delhi NCR, Ayush works as a Business Consultant, focusing on rural livelihoods and environmental sustainability. Since the inception of his NGO, he has played a pivotal role in shaping its mission. His work is driven by the vision that, with the right opportunities and support, every individual can contribute to building a more just and equitable world.`,
+                linkedIn: 'https://www.linkedin.com/in/ayush-mondal/',
+                facebook: 'https://www.facebook.com/calcuttansoul/',
+                instagram: 'https://www.instagram.com/calcuttan_soul/'
             },
             {
-                name: 'Binita Ghosh',
-                position: 'Joint Secretary',
-                imageUrl: 'https://media.licdn.com/dms/image/v2/D5603AQGmgkxLK4wsNA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1666119526617?e=2147483647&v=beta&t=19xjnZFlECs2kfV1ylJu3flBzSIyi5wvfntHUTC46ys',
-                description: `I am Binita Ghosh, one of the co-founders and Joint Secretary of Rangmashal Foundation. Currently I am working as a technical consultant in a private company in Kolkata. I was always keen to do something for the people, for the society. This urge motivated me to be a part of RMF from beginning till now. The great contribution of RMF to the society is beyond expectations. It is a pleasure to be a part of such foundation which is working so hard and successfully making a difference in the society for the underprivileged children. I will always try my best to help RMF grow more and more in every possible perspective.`,
-                linkedIn: 'https://www.linkedin.com/in/binitaghosh/',
-                facebook: 'https://www.facebook.com/binita.ghosh.733',
-                instagram: 'https://www.instagram.com/bini.katha/?hl=en'
+                name: 'Snehesh Guha Thakurta',
+                position: 'Director of Cultural Affairs ',
+                imageUrl: 'https://res.cloudinary.com/dgwgnfulm/image/upload/v1727534365/Website/partners-logo/members-image/snehesh-guha-thakurta.png',
+                description: `Currently after graduation preparing for Govt jobs. And also a professional tabla player.`,
+                linkedIn: '',
+                facebook: 'https://www.facebook.com/share/kqxormUnEEyKoCYD/?mibextid=qi2Omg',
+                instagram: ''
             },
             {
-                name: 'Moutushi Mitra',
-                position: 'Resource Allocation',
-                imageUrl: '',
-                description: `I am a sincere, honest, and hardworking individual now engaged with PNB Metlife India as an Insurance Distribution Manager. I lead a team of insurance advisors and am responsible for driving agency channel sales through my team. As director of Resource Mobilization at Rangmashal, I am responsible for generating funds, finding and mobilizing skilled personnel for social projects, and networking with other organizations that are willing to support our cause.`,
-                linkedIn: 'https://www.linkedin.com/in/moutushi-mitra-57093b52/',
-                facebook: 'https://www.facebook.com/moutushi.mitra.52',
-                instagram: 'https://www.instagram.com/moutushi.1703/'
+                name: 'Ananya Bhowmick ',
+                position: 'Director of Cultural Affairs',
+                imageUrl: 'https://res.cloudinary.com/dgwgnfulm/image/upload/v1727535354/Website/partners-logo/members-image/ananya-bhowmick.png',
+                description: `Hello, this is Ananya Bhowmick. I am professional Kathak dancer & an Instructor based in Kolkata. Currently, I am working as a dance instructor in Rangmashal Foundation.`,
+                linkedIn: '',
+                facebook: '',
+                instagram: 'https://www.instagram.com/ananya_bhowmick_official?igsh=bGtqcjIyeGx1ajVv'
             },
             {
-                name: 'Moutushi Mitra',
-                position: 'Resource Allocation',
-                imageUrl: '',
-                description: `I am a sincere, honest, and hardworking individual now engaged with PNB Metlife India as an Insurance Distribution Manager. I lead a team of insurance advisors and am responsible for driving agency channel sales through my team. As director of Resource Mobilization at Rangmashal, I am responsible for generating funds, finding and mobilizing skilled personnel for social projects, and networking with other organizations that are willing to support our cause.`,
-                linkedIn: 'https://www.linkedin.com/in/moutushi-mitra-57093b52/',
-                facebook: 'https://www.facebook.com/moutushi.mitra.52',
-                instagram: 'https://www.instagram.com/moutushi.1703/'
+                name: 'Turja Chowdhury',
+                position: 'Co - founder/ Asst. Treasurer ',
+                imageUrl: 'https://res.cloudinary.com/dgwgnfulm/image/upload/v1727535856/Website/partners-logo/members-image/turja-chowdhury.png',
+                description: `Turja, a postgraduate in Philosophy from Jadavpur University, is a passionate advocate for the Rangmashal Foundation's grassroots initiatives. Currently pursuing a PGDM in Journalism, Turja’s deep understanding of societal complexities drives his active involvement in creating meaningful social change. With a strong commitment to bridging academic insights and practical efforts, he dedicates himself to building a more equitable and informed future.`,
+                linkedIn: '',
+                facebook: '',
+                instagram: ''
+            },
+            {
+                name: 'Moutushi Mitra ',
+                position: 'Director of Communications',
+                imageUrl: 'https://res.cloudinary.com/dgwgnfulm/image/upload/v1727536409/Website/partners-logo/members-image/moutushi-mitra.png',
+                description: `Moutushi Mitra has been associated with Rangmashal Foundation since April 2023. Initially starting off as a teacher at our Garfa centre and then at Garia, she now takes part in mobilisation of resources, project planning and communications. A retired IT professional and currently working in the financial sector, Moutushi is keenly invested in the cause of social upliftment and works to empower the  women and children from marginalized sections of our society.`,
+                linkedIn: 'https://www.linkedin.com/in/moutushi-mitra-57093b52?trk=contact-info',
+                facebook: 'https://www.facebook.com/moutushi.mitra.52?mibextid=ZbWKwL',
+                instagram: 'https://www.instagram.com/moutushi.1703?igsh=MWpmYms3eGR6eDRtMQ=='
             },
         ]
     }
